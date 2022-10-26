dda365c19871c5aff50d47eeb04373ca95009002
Gustavo33 <guch641@gmail.com>
2022-10-26 23:10:10 +0000

    lint everything

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const Logger = require("./lib/Logger.js")
-const LoggerFile = require("./lib/LoggerDatabase.js")
+const Logger = require("./lib/Logger.js");
+const LoggerFile = require("./lib/LoggerDatabase.js");
 
-module.exports = Logger
-module.exports.Logger = Logger
-module.exports.LoggerFile = LoggerFile
+module.exports = Logger;
+module.exports.Logger = Logger;
+module.exports.LoggerFile = LoggerFile;
+
 /**
  * @param {import("./lib/Logger").config} config 
  * @param {string} logFilePath
  */
 module.exports.createLogger = function(config, logFilePath){
-    const file = new LoggerFile(logFilePath)
-    return new Logger(config, file)
+    const file = new LoggerFile(logFilePath);
+    return new Logger(config, file);
 }
